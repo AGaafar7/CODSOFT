493640e286f8904aa53f05fde39fcecc3d7c1a3b
Ahmed Gaafar <96386423+AGaafar7@users.noreply.github.com>
2024-08-02 05:14:01 +0300

    Update recipe_service.dart
    adding values

--- TaskFive/recipe_finder/lib/network/recipe_service.dart
+++ TaskFive/recipe_finder/lib/network/recipe_service.dart
@@ -7,8 +7,8 @@ import 'service_interface.dart';
 part 'recipe_service.chopper.dart';
 
 //2
-const String apiKey = '{yourKey}';
-const String apiId = '{yourId}';
+const String apiKey = '6dcd71a43b5c3339be8745f2b337546f';
+const String apiId = '0eafb548';
 //3
 const String apiUrl = 'https://api.edamam.com';
 
